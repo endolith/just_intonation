60a3c5ed92233103f9aa059f786cfd2dfce590d2
endolith <endolith@gmail.com>
2023-01-31 22:05:00 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='just_intonation',
+    version='0.1',
+    url='https://github.com/endolith/just_intonation.git',
+    author_email='endolith@gmail.com',
+    description='Classes for Just intonation music theory experiments',
+    py_modules=['just_intonation']
+)
